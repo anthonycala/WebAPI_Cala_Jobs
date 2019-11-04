ca70b92e071db29f5769bb70f01157f8909c4b48
unknown <antcala@icloud.com>
2019-11-04 11:42:35 -0600

    added the seed data

--- WebAPISample/Migrations/Configuration.cs
+++ WebAPISample/Migrations/Configuration.cs
@@ -14,6 +14,13 @@ namespace WebAPISample.Migrations
 
         protected override void Seed(WebAPISample.Models.ApplicationDbContext context)
         {
+            context.Movies.AddOrUpdate(
+            new Models.Movies { Title = "The Departed", Genre = "Drama", DirectorName = "Martin Scorsese" },
+            new Models.Movies { Title = "The Dark Knight", Genre = "Drama", DirectorName = "Christopher Nolan" },
+            new Models.Movies { Title = "Inception", Genre = "Drama", DirectorName = "Christopher Nolan" },
+            new Models.Movies { Title = "Pineapple Express", Genre = "Comedy", DirectorName = "David Gordon Green" },
+            new Models.Movies { Title = "Die Hard", Genre = "Action", DirectorName = "John McTiernan" }
+            ); 
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
